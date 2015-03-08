c373351ddd518aad76e9e7001b6309c972560f62
Anime4000 <ilham92_sakura@yahoo.com>
2015-03-09 00:30:36 +0800

    add CEDICT capabilities

---
+++ py2km.api/CedictProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace py2km.api
+{
+	class CedictProcessor
+	{
+		// Make it outside, load once, to RAM
+		public static Dictionary<String, CedictData> CEDICT = new System.Collections.Generic.Dictionary<string, CedictData>();
+		public static void Load()
+		{
+			CedictData CedictContent = new CedictData()
+			{
+				Pinyin = null,
+				English = null
+			};
+
+			CEDICT.Add("", CedictContent);
+		}
+
+		// Below to search and retrive
+	}
+
+	public class CedictData
+	{
+		public string Pinyin { get; set; }
+		public string English { get; set; }
+	}
+}
